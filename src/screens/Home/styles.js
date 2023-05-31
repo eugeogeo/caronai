0fe22c2b5154b817f098a0d5cb187c575420f27f
Elias Lima <sci.eliaslima@gmail.com>
2023-05-31 18:54:15 -0300

    Create styles.js of home page

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background-color: #F5F8F9;
+    flex: 1;
+`;
+
+export const SearchWrapper = styled.View`
+    width: 100%;
+    background-color: #FFFFFF;
+    padding: 50px 16px 16px 16px;
+    border-bottom-width: 1px;
+    border-color: #E9E7E6;
+`;
+
+export const CardsWrapper = styled.View`
+    flex: 1;
+    padding: 0px 16px;
+`;
+
+export const AvailableRides = styled.View`
+    width: 100%;
+    padding: 8px 16px;
+`;
+
+export const AvailableRidesTitle = styled.Text`
+    font-size: 24px;
+    color: #6F8B90;
+`;
+
+export const WarningWrapper = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
