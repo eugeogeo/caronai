62059336bf15d3be8c2e2f035c0996fee3b4a663
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-05-31 15:47:17 -0300

    feat: adicionando styles do container

---
+++ components/DefaultButton/styles.js
@@ -0,0 +1,12 @@
+import { ActivityIndicator, TouchableOpacity } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #173ea5;
+  border-radius: 50px;
+  padding: 16px 0px;
+  flex-direction: row;
+`;
