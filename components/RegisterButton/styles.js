516a3fe9b850e2e2f6a16c3cafc4cee41b20fa10
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-05-31 16:49:07 -0300

    feat: criado styles do container

---
+++ components/RegisterButton/styles.js
@@ -0,0 +1,11 @@
+import { TouchableOpacity } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  padding: 16px 0px;
+`;
